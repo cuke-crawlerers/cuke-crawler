0f403ffac50e46b4c4fe65bcd69297db3d4fb648
Jevon Wright <jevon@powershop.co.nz>
2015-10-22 11:46:20 +1300

    Check that we have started the dungeon, at least

--- features/step_definitions/quest_steps.rb
+++ features/step_definitions/quest_steps.rb
@@ -1,9 +1,11 @@
 Given(/^I am at the entrance to the (\w+ \w+) dungeon$/) do |name|
   @dungeon = CukeCrawler::Dungeon.generate(name)
   @adventurer = CukeCrawler::Adventurer.new(@dungeon)
+  @started = true
 end
 
 Then(/^my quest is complete$/) do
+  expect(@started).to be_true, "You never started at the entrance"
   expect(@adventurer).to be_alive, "You are dead"
   expect(@adventurer.location).to eq(@dungeon.goal), "You are still stuck in the dungeon"
 end
