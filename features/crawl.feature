70b401bf9fc887f091ab9a6ad2b830dc221bd994
Jevon Wright <jevon@powershop.co.nz>
2015-10-22 11:59:35 +1300

    Issue #2 add step definitions for moving in directions

--- features/crawl.feature
+++ features/crawl.feature
@@ -5,4 +5,5 @@ Feature: A quest
 
   Scenario: Crawl the Acid Gate dungeon
     Given I am at the entrance to the Acid Gate dungeon
+    And I go north
     Then my quest is complete
--- features/step_definitions/quest_steps.rb
+++ features/step_definitions/quest_steps.rb
@@ -4,8 +4,13 @@ Given(/^I am at the entrance to the (\w+ \w+) dungeon$/) do |name|
   @started = true
 end
 
+When(/^I go (\w+)$/) do |direction|
+  expect(@adventurer.location.send(direction)).to_not be_nil, "You ran into a wall to the #{direction}"
+  @adventurer.send("go_#{direction}!")
+end
+
 Then(/^my quest is complete$/) do
-  expect(@started).to be_true, "You never started at the entrance"
+  expect(@started).to eq(true), "You never started at the entrance"
   expect(@adventurer).to be_alive, "You are dead"
   expect(@adventurer.location).to eq(@dungeon.goal), "You are still stuck in the dungeon"
 end
